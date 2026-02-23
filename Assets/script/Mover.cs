94d7e4784fb39377bdb11e2d5e641bac0bbaacda
CharlesSilbergleith <csilbergleith89567@uat.edu>
2026-02-22 19:51:57 -0700

    removed the duplcate

--- Assets/script/Mover.cs
+++
@@ -1,8 +0,0 @@
-using UnityEngine;
-
-public abstract class Mover : MonoBehaviour
-{
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    public abstract void Move(Vector2 moveDircetion);
-    public abstract void Rotate(Vector2 rotateDircetion);
-}
